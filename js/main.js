e4ff3438cf3fcb1302705a459786e605b2b3939b
Danil <2005cramar.05@gmail.com>
2020-10-16 18:28:15 +0400

    awf

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
 	$('.modalDialog').on('click', function(e){
 		if($(e.target).is($('.modalDialog'))){
 			$('.response').text('');
@@ -76,4 +77,74 @@ function register_teacher() {
 	$.ajax(settings).done(function (response) {
 	  console.log(response);
 	});
+}
+
+function login_teacher(){
+	var settings = {
+	  "url": "api/public/api/auth",
+	  "method": "POST",
+	  "timeout": 0,
+	  "headers": {
+	    "Content-Type": "application/json"
+	  },
+	  "data": JSON.stringify({"role":"teacher",
+	  						  "email": $('#email_t_log').val(),
+	  						  "password": $('#password_t_log').val()}),
+	  statusCode: {
+			200: function(response){
+				location.href = "../"
+				localStorage.setItem("token", response.data.token);
+				location.href = "http://localhost/portfolio/personal/person.html";
+				$('.response').text('Вы зашли');
+				$('.response').css('display', 'flex');
+				$('.response').css('color', 'green');
+			},
+			401: function(response){
+				$('.response').text('Неверный Email или пароль');
+				$('.response').css('display', 'flex');
+				$('.response').css('color', 'red');
+			}
+		}
+	};
+	
+	$.ajax(settings).done(function (response) {
+	  console.log(response);
+	});
+}
+function login_student(){
+	var settings = {
+	  "url": "api/public/api/auth",
+	  "method": "POST",
+	  "timeout": 0,
+	  "headers": {
+	    "Content-Type": "application/json"
+	  },
+	  "data": JSON.stringify({"role":"student",
+	  						  "email": $('#email_stud_log').val(),
+	  						  "password": $('#password_stud_log').val()}),
+	  statusCode: {
+			200: function(response){
+				console.log("1");
+				localStorage.setItem("token", response.data.token);
+				location.href = "http://localhost/portfolio/personal/person.html";
+				$('.response').text('Вы зашли');
+				$('.response').css('display', 'flex');
+				$('.response').css('color', 'green');
+			},
+			401: function(response){
+				$('.response').text('Неверный Email или пароль');
+				$('.response').css('display', 'flex');
+				$('.response').css('color', 'red');
+			}
+		}
+	};
+	
+	$.ajax(settings).done(function (response) {
+	  console.log(response);
+	});
+}
+
+function exit(){
+	localStorage.removeItem('token');
+	location.href = "localhost/";
 }
