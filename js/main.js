a527f5b74f761757acebca2dab321c5cba70c8d0
Danil <2005cramar.05@gmail.com>
2020-10-16 17:21:45 +0400

    aawg

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$('.modalDialog').on('click', function(e){
 		if($(e.target).is($('.modalDialog'))){
+			$('.response').text('');
 			location.href = "#close";
 		}
 	})
@@ -15,7 +16,7 @@ function register_student() {
 	    "Content-Type": "application/json"
 	  },
 	  "data": JSON.stringify({"first_name": $('#firstname_stud').val(),
-	  						  "last_name": $('#lastname_stud').val(),
+	  						  "last_name": $('#surname_stud').val(),
 	  						  "middle_name": $('#pastname_stud').val(),
 	  						  "school_number": $('#school_stud').val(),
 	  						  "email": $('#email_stud').val(),
@@ -26,11 +27,49 @@ function register_student() {
 	
 		statusCode: {
 			204: function(response){
-				location.href = "portfolio/";
+				$('.response').text('Вы зарегистрировались');
+				$('.response').css('display', 'flex');
+				$('.response').css('color', 'green');
 			},
 			422: function(response){
-				location.href = "portfolio/error/error.html";
-				$('#which_error').text() = "На данную почту уже зарегестрирован аккаунт";
+				$('.response').text('Неверный Email');
+				$('.response').css('display', 'flex');
+				$('.response').css('color', 'red');
+			}
+		}
+	};
+	$.ajax(settings).done(function (response) {
+	  console.log(response);
+	});
+}
+
+function register_teacher() {
+	var settings = {
+	  "url": "api/public/api/register",
+	  "method": "POST",
+	  "timeout": 0,
+	  "headers": {
+	    "Content-Type": "application/json"
+	  },
+	  "data": JSON.stringify({"first_name": $('#firstname_t').val(),
+	  						  "last_name": $('#surname_t').val(),
+	  						  "middle_name": $('#pastname_t').val(),
+	  						  "school_number": $('#school_t').val(),
+	  						  "email": $('#email_t').val(),
+	  						  "password": $('#password_t').val(),
+	  						  "position": $('#job_t').val(),
+	  						  "role":"teacher"}),
+	
+		statusCode: {
+			204: function(response){
+				$('.response').text('Вы зарегистрировались');
+				$('.response').css('display', 'flex');
+				$('.response').css('color', 'green');
+			},
+			422: function(response){
+				$('.response').text('Неверный Email');
+				$('.response').css('display', 'flex');
+				$('.response').css('color', 'red');
 			}
 		}
 	};
