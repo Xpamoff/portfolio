70b6068118ccd382e4afe3dc5d363b31102915dd
Danil <2005cramar.05@gmail.com>
2020-10-17 14:22:11 +0400

    21312

--- js/view_award.js
+++ js/view_award.js
@@ -7,31 +7,32 @@ $(document).ready(function() {
 	    "Authorization": "Bearer " + localStorage.getItem("token")
 	  },
 	  statusCode: {
-			 403: function(response){
+			 200: function(response){
 					for(var i = 0;i<response.data.length;i++){
-						var one = $('<div class="award_cont">');
-						var two = $('<div class="award_img">');
-						var three = $('<img src="../api/storage/app/' + response[i].data["img"] + '">');
+						var one = $('<div class="award_cont"></div>');
+						var two = $('<div class="award_img"></div>');
+						var three = $('<img src="../api/storage/app/' + response.data[i].img + '">');
 						var four = $('</div>');
-						var five = $('<div class="rate_list">');
-						if(response[i].data["place"] != 0){
-							var six = $('<h4>' + response[i].data["place"] + ' место</h4>');
+						var five = $('<div class="rate_list"></div>');
+						if(response.data[i].place != 0){
+							var six = $('<h4>' + response.data[i].place + ' место</h4>');
 						}
 						else{
 							var six = $('<h4>Участник</h4>');
 						}
-						var seven = $('<p>' + response[i].data["event"] + '</p>');
+						var seven = $('<p>' + response.data[i].event + '</p>');
 						var eight = $('</div>');
 						var nine = $('</div>');
 						$('.cont').append(one);
-						$('.cont').append(two);
-						$('.cont').append(three);
-						$('.cont').append(four);
-						$('.cont').append(five);
-						$('.cont').append(six);
-						$('.cont').append(seven);
-						$('.cont').append(eight);
-						$('.cont').append(nine);
+						one.append(two.append(three));
+						//$('.cont').append(three);
+						//$('.cont').append(four);
+						one.append(five);
+						five.append(six);
+						five.append(seven);
+						//$('.cont').append(seven);
+						//$('.cont').append(eight);
+						//$('.cont').append(nine);
 						// <div class="award_cont">
 								// <div class="award_img">
 									// <img src="../assets/news_1.jpg">
